4c275303e04e7921156f5940a352a2ade8c0d3f3
JMONDESIR <Joey.M.Dev@gmail.com>
2019-03-05 11:53:55 -0600

    first commit

---
+++ public/scripts/main.js
@@ -0,0 +1,7 @@
+console.log("connected")
+const conjunction = function (firstWord, secondWord) {
+        const conjoinedWord = `${firstWord} ${secondWord}`
+        console.log(conjoinedWord)
+}
+
+conjunction("Master", "Card")
